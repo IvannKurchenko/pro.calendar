3e488724d3e2561c74038d06fa902215cb6db8b2
IvannKurchenko <ivankoorchenko@gmail.com>
2021-10-25 06:36:21 +0300

    experiments

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{now}}</h1>
+  <h1>{{month}}</h1>
+  <h1>{{weekday}}</h1>
+  <h1>{{weekNumber}}/{{weeksInWeekYear}}</h1>
+  <h1>{{quarter}}</h1>
+
+  <div class="columns">
+    <div class="column is-one-quarter">quarter 1 months column</div>
+    <div class="column is-one-quarter">quarter 2 months column</div>
+    <div class="column is-one-quarter">quarter 2 months column</div>
+    <div class="column is-one-quarter">quarter 2 months column</div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    const { DateTime } = require("luxon");
+
+    let dateTime = DateTime.now();
+    let month = dateTime.month;
+    let weekNumber = dateTime.weekNumber;
+    let weeksInWeekYear = dateTime.weeksInWeekYear;
+    let quarter = dateTime.quarter;
+
+
+    return {
+      now: DateTime.now(),
+      month: month,
+      weekNumber: weekNumber,
+      weeksInWeekYear: weeksInWeekYear,
+      weekday: dateTime.weekday,
+      quarter: quarter
+    }
   }
+  /*components: {
+    HelloWorld
+  }*/
 }
 </script>
 
 <style>
-#app {
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css";
+#{
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
